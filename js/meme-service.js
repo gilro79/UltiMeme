38ac8ccf2fe7b94fdef5ce9496e0e59ae077620b
Roy Gil <gilro79@gmail.com>
2021-10-27 12:26:16 +0300

    adding gallery

--- js/meme-service.js
+++ js/meme-service.js
@@ -1,3 +1,15 @@
 'use strict'
 
 
+var gMeme = {
+    imgId : 1,
+    lineIndex: 0,
+    lines: [
+        {
+            txt: 'what am I doing?',
+            size: 25,
+            align: 'center',
+            color: 'red'
+        }
+    ]
+}
